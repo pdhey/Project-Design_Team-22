b8b8ed9abb564595e06664cf999a90b8bcec771e
pdhey <pd.squieta@gmail.com>
2024-11-04 13:23:02 +0800

    Update App.js

--- fish-silage-web-app/App.js
+++ fish-silage-web-app/App.js
@@ -44,7 +44,7 @@ export default function App() {
           }} 
         />
         <Tab.Screen 
-          name="Dashboard" 
+          name="Main" 
           component={Layout} 
           options={{
             tabBarLabel: ({ focused }) => (
